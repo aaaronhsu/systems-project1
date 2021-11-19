b7e9c73484563809c233bc24c45547d0fce8d369
Eliza <ekoknapp@gmail.com>
2021-11-19 10:11:47 -0500

    added an infinite terminal

--- main.c
+++ main.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
-#include <time.h>
-#include <sys/wait.h>
+#include "headers.h"
 
 // for now the max args it takes is 5 if you input during the prompt
 #define ARG_NUM 5
@@ -14,25 +6,34 @@
 char ** parse_args(char *line);
 
 int main(int argc, char *argv[]) {
-  // argc - 1 is the amount of args in command line
+  int exit = 1;
+  printf("Hello! Welcome to our shell. Type \"exit\" to leave.\n");
+  // for command line input if it exists
   char ** args = calloc(ARG_NUM, sizeof(char *));
   if (argc >= 2) {
-    // you have the args now loop through and add them
-    int i;
-    for (int i = 1; i < ARG_NUM; i++) {
-      args[i - 1] = argv[i];
-    }
-  } else {
+      // you have the args now loop through and add them
+      int i;
+      for (int i = 1; i < ARG_NUM; i++) {
+        args[i - 1] = argv[i];
+      }
+  }
+  // so that it goes on forever until exited
+  while(exit) {
+    args = calloc(ARG_NUM, sizeof(char *));
     char input[100];
     printf("Enter a command for the shell: ");
     fgets(input, 100, stdin);
     input[strcspn(input, "\n")] = 0; // gets rid of the newline
     args = parse_args(input);
-  }
 
-  // test the two methods of assigning args
-  for (int i = 0; i < ARG_NUM; i++) {
-    printf("%s\n", args[i]);
+    // TESTING the two methods of assigning args
+    for (int i = 0; i < ARG_NUM; i++) {
+      printf("%s\n", args[i]);
+    }
+    // exit function 
+    if (!strcmp(args[0], "exit")) {
+      exit = 0;
+    }
   }
 
   return 0;
@@ -45,6 +46,7 @@ char ** parse_args(char *line) {
   for (int i = 1; i < ARG_NUM; i++) {
     if (strsep(&line, " ") != NULL) args[i] = line;
   }
+  // TODO: edge case- if you put a space as the last arg it takes up a slot
   return args;
 }
  
