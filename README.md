b4434f723c9b3864dd630b6837492279bc269166
Aaron Hsu <aaronhsu4606@gmail.com>
2021-11-26 23:40:11 -0500

    running on school computer

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - Run commands one at a time
 - Run commands separated by ` ;`
   - There must be a space before the semicolon
+- Redirection using `>` and `<`
+  - You cannot use them together in the same command (yet)
 
 ---
 
@@ -12,7 +14,5 @@
 - Redirection
   - After creating the `args` array for a command input, scan for `>` and `<`
     - If `>`, take the argument before the `>` and store it in a file using `dup2(file_name, 1);`
-    - Close the file using `close(file_pointer)`
     - Execute as normal
     - If `<`, do the same thing but take the argument after the `<` and use `dup2(file_name, 0);`
-    - **I have no idea if this works lol**
--- functions.c
+++ functions.c
@@ -76,11 +76,14 @@ int execute_args(char **args) {
   // count the number of args for later
   int arg_count = 0;
   int contains_greater = -1; // >
-  int contains_less = -1; // >>
+  int contains_less = -1; // <
 
   while (args[arg_count] != NULL) {
+
+    // scan for > and <
     if (strcmp(args[arg_count], ">") == 0) contains_greater = arg_count;
     if (strcmp(args[arg_count], "<") == 0) contains_less = arg_count;
+    
     arg_count++;
   }
 
@@ -102,22 +105,33 @@ int execute_args(char **args) {
   // other
   else {
 
+    // creates backup of stdout and stdin so we can restore them later
     int out, in;
     int backup_sdout = dup(STDOUT_FILENO);
     int backup_sdin = dup(STDIN_FILENO);
 
     if (contains_greater != -1) {
       // >
+
+      // creates file to write to
       out = open(args[contains_greater + 1], O_WRONLY | O_CREAT, 0644);
+
+      // makes the command stop at the >
       args[contains_greater] = '\0';
 
+      // redirects stdout to the file
       dup2(out, STDOUT_FILENO);
     }
     else if (contains_less != -1) {
       // <
+
+      // creates file to read from
       in = open(args[contains_less + 1], O_RDONLY);
+
+      // makes the command stop at the <
       args[contains_less] = '\0';
 
+      // redirects stdin to the file
       dup2(in, STDIN_FILENO);
     }
 
